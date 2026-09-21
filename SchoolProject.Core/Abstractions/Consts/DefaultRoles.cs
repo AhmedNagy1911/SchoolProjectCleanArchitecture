b9f985b6656c301215f80c00d18ebd3621d045c9
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-21 21:14:13 +0300

    Add DefaultRoles

---
+++ SchoolProject.Core/Abstractions/Consts/DefaultRoles.cs
@@ -0,0 +1,22 @@
+﻿namespace SchoolProject.Core.Abstractions.Consts;
+
+public static class DefaultRoles
+{
+    public const string Admin = nameof(Admin);
+    public const string AdminRoleId = "0191a4b6-c4fc-752e-9d95-40b5e4e68054";
+    public const string AdminRoleConcurrencyStamp = "0191a4b6-c4fc-752e-9d95-40b631d1866d";
+
+
+    public const string Teacher = nameof(Teacher);
+    public const string MemberRoleId = "0191a4b6-c4fc-752e-9d95-40b7a5cb88f0";
+    public const string MemberRoleConcurrencyStamp = "0191a4b6-c4fc-752e-9d95-40b85cf3fd22";
+
+    public const string Parent = nameof(Parent);
+    public const string ParentRoleId = "4315061C-AF52-4C13-A9EF-ED41A5941362";
+    public const string ParentRoleConcurrencyStamp = "4F9BD8C2-CD96-4CB5-BBD4-3249D2A6AB9C";
+
+    public const string Student = nameof(Student);
+    public const string StudentRoleId = "54201DC0-4E33-45A3-819E-2CD103888BE0";
+    public const string StudentRoleConcurrencyStamp = "1279C76D-2C22-4D57-B453-91D5C5B2F8E0";
+
+}
